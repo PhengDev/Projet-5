4ccd1f9fe83e054aa8b44f33b24372e797a3ff14
Pheng Power <pheng.ly30@gmail.com>
2019-08-12 21:00:01 +0200

    Première création dans la base de donné et ajout d'un contenu à partir de la base de donné

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,19 +2,58 @@
 
 namespace App\Controller;
 
+
+use App\Repository\PropertyRepository;
+use App\Entity\Property;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PropertyController extends AbstractController
 {
+
+    /**
+     * @var PropertyRespository
+     */
+    private $respository;
+
+    public function __construct(PropertyRepository $respository, ObjectManager $em)
+    {
+        $this->respository = $respository;
+        $this->em = $em;
+    }
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
+
     public function index(): Response
     {
+        $property = $this->respository->findAllVisible();
+        dump($property);
+        $this->em->flush();
         return $this->render("property/index.html.twig", [
+            'current_menu' => 'properties'
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show",requirements={"slug": "[a-z0.-9\-]*"})
+     * @param Property $property
+     * @return Response
+     */
+    public function show(Property $property, string $slug): Response
+    {
+        if ($property->getSlug() !== $slug){
+            return $this->redirectToRoute('properties.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+
+        }
+        return $this->render('property/show.html.twig',[
+            'property' => $property,
             'current_menu' => 'properties'
         ]);
     }
---
+++ src/Entity/Property.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ */
+class Property
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+
+    /**
+     * @ORM\Column(type="boolean", options={"default": false})
+     */
+    private $sold = false;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+
+    public function __construct()
+    {
+        $this -> created_at = new \DateTime();
+    }
+   
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+   public function getSlug(): string
+    {
+        return (new Slugify())->slugify($this->title);
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function getFormatedPrice(): string
+    {
+        return number_format($this->price, 0, '', ' ');
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSold(): ?bool
+    {
+        return $this->sold;
+    }
+
+    public function setSold(bool $sold): self
+    {
+        $this->sold = $sold;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+ 
+}
